3fb0bb512f3f321189ecb3174ec0f32a086323d7
Turan Gozukara <117595686+turangozukara@users.noreply.github.com>
2023-12-25 17:08:57 +0300

    app.py modified

--- phonebook/phonebook-app.py
+++ phonebook/phonebook-app.py
@@ -1,5 +1,3 @@
-# To show this app works fine, we need to create a RDS instance at first.
-
 # Import Flask modules
 # As we know, we are gonna import necessary libraries. We've also imported 
 from flask import Flask, request, render_template
