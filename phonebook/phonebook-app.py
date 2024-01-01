9b78279807d3a1fcef643a0de55dc8b4cd2b0cba
Turan Gozukara <117595686+turangozukara@users.noreply.github.com>
2024-01-01 12:32:09 +0300

    phonebook-app.py modified

--- phonebook/phonebook-app.py
+++ phonebook/phonebook-app.py
@@ -114,7 +114,7 @@ def add_record():
     else:
         return render_template('add-update.html', show_result=False, not_valid=False, action_name='save', developer_name='Turan')
 
-# using template files named `add-update.html` given under `templates` folder
+# using template
 # and assign to the static route of ('update')
 @app.route('/update', methods=['GET', 'POST'])
 def update_record():
