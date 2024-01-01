f0ba907b0d701bfe683d9fe1f231cbf536d3e313
Turan Gozukara <117595686+turangozukara@users.noreply.github.com>
2024-01-01 12:31:56 +0300

    phonebook-app.py modified

--- phonebook/phonebook-app.py
+++ phonebook/phonebook-app.py
@@ -133,7 +133,7 @@ def update_record():
     else:
         return render_template('add-update.html', show_result=False, not_valid=False, action_name='update', developer_name='Turan')
 
-# Write a function named `delete_record` which updates the record in the db using `GET` and `POST` methods,
+# Write a function named `delete_record`
 # using template files named `delete.html` given under `templates` folder
 # and assign to the static route of ('delete')
 @app.route('/delete', methods=['GET', 'POST'])
